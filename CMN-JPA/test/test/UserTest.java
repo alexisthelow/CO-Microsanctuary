8170464d1c1c0e7b2406f708893b784b740de51a
Alexis Low <alexisthelow@gmail.com>
2017-12-11 13:23:54 -0700

    project folder rename

--- CMN-JPA/test/test/UserTest.java
+++ CMN-JPA/test/test/UserTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
